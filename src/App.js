e7583307072d7fd0eb01b7d78fc5fe155574ec60
lucaswychan <chanconan@gmail.com>
2024-11-18 23:24:26 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // App.js
 import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import TabNavigation from "./components/TabNavigation";
 import TabContent from "./components/TabContent";
