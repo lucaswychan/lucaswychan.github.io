f55a31b4aaeb08b8e6e546a335084404a17f356c
lucaswychan <chanconan@gmail.com>
2025-02-02 16:54:54 +0800

    refrfactor: modify the description of myself

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,8 +6,7 @@ function Header() {
         <header className="mb-5">
             <h1 className="mb-3">Lucas Wun-Yu Chan</h1>
             <p className="lead">
-                MPhil in Computer Science | HKUST | Artificial Intelligence |
-                Quantitative Research
+                MPhil in Computer Science | HKUST | Machine Learning | Quant
             </p>
         </header>
     );
