008c8b8ea20590a6aff200383a7c44629948a607
lucaswychan <chanconan@gmail.com>
2025-05-15 16:10:14 +0800

    update to latest

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -5,60 +5,60 @@ import '../styles/Gallery.css';
 
 // Using placeholder images from Unsplash for demo purposes
 const galleryImages = [
-  { 
-    id: 1, 
-    src: 'images/gallery/fuji_mountain.jpeg', 
-    alt: 'Fuji Mountain', 
-    caption: 'Fuji Mountain during my trip to Japan at Jan' 
-  },
-  { 
-    id: 2, 
-    src: 'images/gallery/dodgeball.jpeg', 
-    alt: 'Dodgeball', 
-    caption: 'Joint-Uni Dodgeball Competition at 2021' 
-  },
-  { 
-    id: 3, 
-    src: 'images/gallery/ocamp.jpeg', 
-    alt: 'Ocamp', 
-    caption: 'ELEC OCamp 2024' 
-  },
-  { 
-    id: 4, 
-    src: 'images/gallery/graduation.jpeg', 
-    alt: 'Graduation', 
-    caption: 'Delivering graduation speech at HKUST' 
-  },
-  { 
-    id: 5, 
-    src: 'images/gallery/japan_ship.jpeg', 
-    alt: 'Japan Ship', 
-    caption: 'Waiting for the ship to sail at Kyoto' 
-  },
-  { 
-    id: 6, 
-    src: 'https://images.unsplash.com/photo-1522202176988-66273c2fd55f?auto=format&w=500&q=60', 
-    alt: 'Study Group', 
-    caption: 'Working on a weekend project with friends' 
-  },
-  { 
-    id: 7, 
-    src: 'https://images.unsplash.com/photo-1532012197267-da84d127e765?auto=format&w=500&q=60', 
-    alt: 'Books on Desk', 
-    caption: 'My favorite reads this month' 
-  },
-  { 
-    id: 8, 
-    src: 'https://images.unsplash.com/photo-1519501025264-65ba15a82390?auto=format&w=500&q=60', 
-    alt: 'City Street', 
-    caption: 'Exploring the city in my free time' 
-  },
-  { 
-    id: 9, 
-    src: 'https://images.unsplash.com/photo-1492684223066-81342ee5ff30?auto=format&w=500&q=60', 
-    alt: 'Friends Dinner', 
-    caption: 'Dinner gathering with friends last weekend' 
-  }
+    {
+        id: 1,
+        src: "images/gallery/fuji_mountain.jpeg",
+        alt: "Fuji Mountain",
+        caption: "Fuji Mountain during my trip to Japan at Jan",
+    },
+    {
+        id: 2,
+        src: "images/gallery/dodgeball.jpeg",
+        alt: "Dodgeball",
+        caption: "Joint-Uni Dodgeball Competition at 2021",
+    },
+    {
+        id: 3,
+        src: "images/gallery/ocamp.jpeg",
+        alt: "Ocamp",
+        caption: "ELEC OCamp 2024",
+    },
+    {
+        id: 4,
+        src: "images/gallery/graduation.jpeg",
+        alt: "Graduation",
+        caption: "Delivering graduation speech at HKUST",
+    },
+    {
+        id: 5,
+        src: "images/gallery/japan_ship.jpeg",
+        alt: "Japan Ship",
+        caption: "Waiting for the ship to sail at Kyoto",
+    },
+    {
+        id: 6,
+        src: "images/gallery/wine.png",
+        alt: "Wine",
+        caption: "I love wine btw.",
+    },
+    {
+        id: 7,
+        src: "images/gallery/project_car.png",
+        alt: "Project Car",
+        caption: "The funniest (and most stressful) project I've ever worked on (during my year 3)",
+    },
+    {
+        id: 8,
+        src: "images/gallery/larp.png",
+        alt: "City Street",
+        caption: "Do you know what LARP is?",
+    },
+    {
+        id: 9,
+        src: "https://images.unsplash.com/photo-1492684223066-81342ee5ff30?auto=format&w=500&q=60",
+        alt: "Friends Dinner",
+        caption: "Dinner gathering with friends last weekend",
+    },
 ];
 
 function Gallery() {
